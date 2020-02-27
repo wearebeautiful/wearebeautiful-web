c3159bdff6d03021bcd35dd96e191d170c32dda5
Robert Kaye <rob@musicbrainz.org>
2020-02-27 20:44:11 +0100

    Start building the compression layer

--- start-containers.sh
+++ start-containers.sh
@@ -6,9 +6,15 @@ docker run -d \
     --expose 3031 \
     --name wearebeautiful-web \
     -v /home/website/wearebeautiful-models:/models \
+    --network=website-network \
+    wearebeautiful.info:beta
+
+docker run -d \
+    --expose 80 \
+    --name wearebeautiful-comp \
     -v $SRC_DIR/admin/nginx/vhost.d:/etc/nginx/vhost.d:ro \
     --env "VIRTUAL_HOST=wearebeautiful.info" \
     --env "LETSENCRYPT_HOST=wearebeautiful.info" \
     --env "LETSENCRYPT_EMAIL=mayhem@gmail.com" \
     --network=website-network \
-    wearebeautiful.info:beta
+    nginx:1.17.8
