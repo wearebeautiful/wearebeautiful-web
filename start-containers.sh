46a0945e4af72892b1823d232479fdae65c212af
Robert Kaye <rob@musicbrainz.org>
2020-03-04 11:49:12 +0000

    Add DOMAIN config

--- start-containers.sh
+++ start-containers.sh
@@ -3,6 +3,7 @@
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LOGDIR=/tmp/
 MODELS=/home/website/wearebeautiful-models
+DOMAIN=silly.wearebeautiful.info
 
 docker run -d \
     --expose 3031 \
@@ -19,13 +20,13 @@ docker run -d \
     -v wab-cache:/cache \
     -v $MODELS:/models:ro \
     -v $SRC_DIR/admin/nginx/cache/cache.conf:/etc/nginx/conf.d/cache.conf:ro \
-    -v $SRC_DIR/admin/nginx/vhost.d/wearebeautiful.info:/etc/nginx/vhost.d/silly.wearebeautiful.info:ro \
+    -v $SRC_DIR/admin/nginx/vhost.d/wearebeautiful.info:/etc/nginx/vhost.d/$DOMAIN:ro \
     -v $SRC_DIR/admin/nginx/compressor/compressor-nginx.conf:/etc/nginx/nginx.conf:rw \
     -v $LOGDIR:/var/log/nginx:rw \
     --network=website-network \
-    --env "LETSENCRYPT_HOST=silly.wearebeautiful.info" \
+    --env "LETSENCRYPT_HOST=$DOMAIN" \
     --env "LETSENCRYPT_EMAIL=mayhem@gmail.com" \
-    --env "VIRTUAL_HOST=silly.wearebeautiful.info" \
+    --env "VIRTUAL_HOST=$DOMAIN" \
     nginx:1.17.8
 
 
