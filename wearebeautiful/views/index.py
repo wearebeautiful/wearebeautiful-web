8a3dbbfc80d8458419dcbf6d64cdfb9d22f44642
Robert Kaye <rob@musicbrainz.org>
2020-04-07 22:51:07 +0200

    Index page view tweaks

--- wearebeautiful/views/index.py
+++ wearebeautiful/views/index.py
@@ -9,6 +9,12 @@ bp = Blueprint('index', __name__)
 
 
 @bp.route('/')
+def soon():
+    return render_template("coming-soon.html", bare=True)
+
+
+@bp.route('/index')
+@auth.login_required
 def index():
     models = DBModel.select(DBModel.model_id, DBModel.code, DBModel.body_part, DBModel.version) \
                     .order_by(DBModel.id.desc()) \
@@ -21,20 +27,10 @@ def index():
 
     return render_template("index.html", recent_models=model_list)
 
-#    if auth.username():
-#        return render_template("index.html")
-#    else:
-#        return redirect(url_for("index.soon"))
-
 
 @bp.route('/browse')
 def browse():
     return redirect(url_for("model.browse_by_part"))
-
-
-@bp.route('/soon')
-def soon():
-    return render_template("coming-soon.html", bare=True)
 
 
 @bp.route('/team')
