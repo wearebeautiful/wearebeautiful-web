d37db5e6fd5df4c813cf3686a5a80f22df841d01
Robert Kaye <rob@musicbrainz.org>
2020-04-21 00:01:55 +0200

    Added staff picks and other improvements to the home page

--- wearebeautiful/views/index.py
+++ wearebeautiful/views/index.py
@@ -14,6 +14,19 @@ bp = Blueprint('index', __name__)
 def soon():
     return render_template("about/coming-soon.html", bare=True)
 
+def load_slide_models(slide_model_ids):
+    slide_models = []
+    for slide in slide_model_ids:
+        m = DBModel.get(DBModel.model_id == slide[0], DBModel.code == slide[1], DBModel.version == slide[2])
+        m.parse_data()
+
+        slide_models.append({
+            "desc" : "%s model %s" % (m.body_part, m.display_code),
+            "screenshot" : url_for_screenshot_m(m),
+            "link" : "/model/" + m.display_code
+        })
+
+    return slide_models
 
 @bp.route('/index')
 @auth.login_required
@@ -28,25 +41,27 @@ def index():
         m.parse_data()
         model_list.append(m)
 
-    slide_models_ids = [
+    sample_model_ids = [
         ("476551", "VLNN", 1),
         ("554268", "PLRN", 1),
         ("320912", "FSAN", 1),
         ("833579", "LSNN", 1)
     ]
+    sample_models = load_slide_models(sample_model_ids)
 
-    slide_models = []
-    for slide in slide_models_ids:
-        m = DBModel.get(DBModel.model_id == slide[0], DBModel.code == slide[1], DBModel.version == slide[2])
-        m.parse_data()
+    picks_model_ids = [
+        ("591522", "FSAN", 1),
+        ("591522", "FSAN", 2),
+        ("591522", "FSAN", 4),
+        ("591522", "FSAN", 5),
+    ]
+    picks_models = load_slide_models(picks_model_ids)
 
-        slide_models.append({
-            "desc" : "%s model %s" % (m.body_part, m.display_code),
-            "screenshot" : url_for_screenshot_m(m),
-            "link" : "/model/" + m.display_code
-        })
-
-    return render_template("index.html", slide_models=slide_models, recent_models=model_list, model_count=model_count)
+    return render_template("index.html", 
+        sample_models=sample_models, 
+        picks_models=picks_models, 
+        recent_models=model_list, 
+        model_count=model_count)
 
 
 @bp.route('/browse')
