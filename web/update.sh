72d1199cf5f883c1ac7bc86e67473e311939dd94
Robert Kaye <rob@musicbrainz.org>
2020-09-02 18:54:25 +0200

    Add clear cache command at the end of update

--- web/update.sh
+++ web/update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo "---- build"
 git pull origin master
 ./build.sh
+
+echo "---- stop containers"
 ./stop-containers.sh
+
+echo "---- start containers"
 ./start-containers.sh
+
+echo "---- clear cache"
+docker exec -it wab-comp admin/clear_cache.sh
