f47bf2636964e859e8dc8f8b9b4c31ca484227c0
Robert Kaye <rob@musicbrainz.org>
2020-09-03 21:23:22 +0200

    Run containers in host mode for the stats node and we should be fine

--- statistics/start-production.sh
+++ statistics/start-production.sh
@@ -12,7 +12,7 @@ echo "---- start telegraf"
 sed 's/%hostname%/'$HOSTNAME'/g' telegraf.conf.in > telegraf.conf
 docker run -d \
     --name telegraf \
-    --network=wab-network \
+    --network=host \
     -e HOST_PROC=/host/proc \
     -v /proc:/host/proc:ro \
     -v `pwd`/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
@@ -23,7 +23,7 @@ echo "---- start grafana"
 docker run -d \
     -p "3000:3000" \
     --name grafana \
-    --network=wab-network \
+    --network=host \
     --restart unless-stopped \
     -v grafana-volume:/var/lib/grafana \
     grafana/grafana:7.1.5
