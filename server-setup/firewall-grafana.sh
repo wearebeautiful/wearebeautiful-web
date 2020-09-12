dc02f33b09edd38cf004ef43526d79a880e0a17a
Robert Kaye <rob@musicbrainz.org>
2020-09-12 22:42:09 +0200

    Final (I hope) firewall tweaks

--- server-setup/firewall-grafana.sh
+++ server-setup/firewall-grafana.sh
@@ -4,6 +4,5 @@
 sed -i 's/^#& stop/\& stop/' /etc/rsyslog.d/20-ufw.conf && systemctl restart rsyslog
 
 ufw enable
-ufw allow from 135.181.45.162 to any port 8086
-ufw allow from 95.217.160.238 to any port 8086
+ufw allow ssh
 ufw status verbose
--- statistics/start-production.sh
+++ statistics/start-production.sh
@@ -41,4 +41,4 @@ docker run -d \
     -v grafana-volume:/var/lib/grafana \
     grafana/grafana:7.1.5
 
-sudo ufw-docker delete allow grafana 3000
+sudo ufw deny tcp/3000
