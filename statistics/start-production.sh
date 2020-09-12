7d61870efdb97b96d743a0532f0f32ca1b28e896
Robert Kaye <rob@musicbrainz.org>
2020-09-12 23:40:36 +0200

    Fix a port deny

--- statistics/start-production.sh
+++ statistics/start-production.sh
@@ -22,8 +22,6 @@ docker run -d \
 sudo ufw allow from 95.216.117.155 to any port 8086
 sudo ufw allow from 135.181.86.222 to any port 8086
 
-95.216.117.155
-
 echo "---- start telegraf"
 docker run -d \
     --name telegraf \
